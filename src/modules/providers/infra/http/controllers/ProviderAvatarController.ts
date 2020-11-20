df291ea75228d7070082267be9cd111933a1614f
erick <erick.piazza@brainweb.com.br>
2020-11-20 18:07:07 -0300

    api avatar provider

---
+++ src/modules/providers/infra/http/controllers/ProviderAvatarController.ts
@@ -0,0 +1,20 @@
+import { Request, Response } from 'express';
+import { container } from 'tsyringe';
+import UpdateProviderAvatarService from '@modules/providers/services/UpdateProviderAvatarService';
+
+export default class ProviderAvatarController {
+  public async update(request: Request, response: Response): Promise<Response> {
+    const updateProviderAvatarService = container.resolve(
+      UpdateProviderAvatarService,
+    );
+    console.log(request.file.filename);
+    const provider = await updateProviderAvatarService.execute({
+      user_id: request.user.id,
+      avatarFileName: request.file.filename,
+    });
+
+    // delete user.password;
+
+    return response.json(provider);
+  }
+}
--- src/modules/providers/infra/http/routes/providers.routers.ts
+++ src/modules/providers/infra/http/routes/providers.routers.ts
@@ -1,12 +1,17 @@
 import { Router } from 'express';
+import multer from 'multer';
+import uploadConfig from '@config/upload';
 
 import ProvidersController from '../controllers/ProviderController';
+import ProviderAvatarController from '../controllers/ProviderAvatarController';
 
 import ensureAuthenticated from '../middlewares/ensureAuthenticated';
 
 const providersRouter = Router();
+const upload = multer(uploadConfig);
 
 const providersController = new ProvidersController();
+const providerAvatarController = new ProviderAvatarController();
 
 providersRouter.post('/', providersController.create);
 
@@ -14,6 +19,12 @@ providersRouter.patch(
   '/profile',
   ensureAuthenticated,
   providersController.update,
+);
+providersRouter.patch(
+  '/avatar',
+  ensureAuthenticated,
+  upload.single('avatar'),
+  providerAvatarController.update,
 );
 
 export default providersRouter;
